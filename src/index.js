e95cf9df9439f45e060fa5890f651f70a7d6c3c0
Vedran Milović <vedran.milovic@gmail.com>
2023-07-09 18:09:45 +0200

    todo_app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css'; 
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './services/reducer';
+
+const store = createStore(reducer);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
